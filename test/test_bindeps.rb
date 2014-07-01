bef5a93be1f9f743dbf7f76b910cce22ef6bbc31
Richard Smith <rds45@cam.ac.uk>
2014-07-01 13:37:53 +0100

    fix tests for non-first-line version

--- test/test_bindeps.rb
+++ test/test_bindeps.rb
@@ -6,8 +6,7 @@ class TestBinDeps < Test::Unit::TestCase
 
     setup do
       test_dir = File.dirname(__FILE__)
-      data_dir = File.join(test_dir, 'data')
-      @test_yaml = File.join(data_dir, 'fakebin.yaml')
+      @data_dir = File.join(test_dir, 'data')
     end
 
     # teardown do
@@ -22,8 +21,24 @@ class TestBinDeps < Test::Unit::TestCase
     # end
 
     should "identify and install missing dependencies" do
-      Bindeps.require @test_yaml
+      test_yaml = File.join(@data_dir, 'fakebin.yaml')
+      Bindeps.require test_yaml
       assert_equal 'success', `fakebin`.strip
+    end
+
+    should "handle case where version is not on first line" do
+      test_yaml = File.join(@data_dir, 'fakebin2.yaml')
+      # install fakebin2
+      Bindeps.require test_yaml
+      # now Dependency should detect it as installed
+      deps = YAML.load_file test_yaml
+      deps.each_pair do |name, config|
+        d = Bindeps::Dependency.new(name,
+                                    config['binaries'],
+                                    config['version'],
+                                    config['url'])
+        assert d.installed?('fakebin2')
+      end
     end
 
   end
